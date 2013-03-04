e58c2c72d493f9b4cca1925a3fdd63411fe9de20
bercium <bercium@gmail.com>
2013-03-04 20:49:52 +0100

    Preparation for CRUD list

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'CRUD List', 'url'=>array('/site/page', 'view'=>'list')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
---
+++ protected/views/site/pages/list.php
@@ -0,0 +1,10 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - CRUD List';
+?>
+<h1>CRUD List</h1>
+
+<p>Tukaj naj bo seznam vseh povezav do avto zgeneriranih CRUDov:</p><br />
+
+<a href="?r=site/contact">Contact</a><br />
