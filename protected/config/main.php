eb5be2bccbd0993b22a21defdba1c8b201af949d
Ziga Berce <bercium@gmail.com>
2013-03-06 15:58:54 +0100

    Setup of audit trail extension

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,6 +22,7 @@ return array(
 		'application.models.*',
 		'application.components.*',
     'ext.giix-components.*', // giix components
+    'application.extensions.auditTrail.models.AuditTrail',
 	),
 
 	'modules'=>array(
@@ -36,7 +37,7 @@ return array(
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		
+    /*'auditTrail'=>array(),*/
 	),
 
 	// application components
--- protected/views/site/pages/list.php
+++ protected/views/site/pages/list.php
@@ -7,4 +7,5 @@ $this->pageTitle=Yii::app()->name . ' - CRUD List';
 
 <p>Tukaj naj bo seznam vseh povezav do avto zgeneriranih CRUDov:</p><br />
 
+<a href="?r=auditTrail">Action Trail</a><br />
 <a href="?r=site/contact">Contact</a><br />
