53a0bb7877dcf8d57a6eb806bbf5dfae2ea34b7d
Ziga Berce <bercium@gmail.com>
2013-03-08 21:56:44 +0100

    Prepared local config for DB and path

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../yii-1.1.13.e9e4a0/framework/yii.php';
+$yii= dirname(__FILE__).require(dirname(__FILE__).'/protected/config/local-path.php');
+$yii=$yii."yii.php";
+//$yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
