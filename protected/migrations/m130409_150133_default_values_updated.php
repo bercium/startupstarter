fc9de7e5631ca2f8be7e82a3b8e099698fb34457
Ziga Berce <bercium@gmail.com>
2013-04-17 19:03:39 +0200

    Idea create finished Fixed sif.language.sql Added idea card to about our project

--- protected/migrations/m130409_150133_default_values_updated.php
+++ protected/migrations/m130409_150133_default_values_updated.php
@@ -5,7 +5,7 @@ class m130409_150133_default_values_updated extends CDbMigration
 	public function up()
 	{
     $this->execute("ALTER TABLE `idea_translation` CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL");
-    $this->execute("ALTER TABLE `slocoworking`.`idea_translation` DROP INDEX `language_code` ,ADD INDEX `language_id` ( `language_id` ) ");
+    $this->execute("ALTER TABLE `idea_translation` DROP INDEX `language_code` ,ADD INDEX `language_id` ( `language_id` ) ");
     $this->execute("ALTER TABLE `click_idea` CHANGE `user_id` `user_id` INT( 11 ) UNSIGNED NULL");
     $this->execute("ALTER TABLE `click_user` CHANGE `user_id` `user_id` INT( 11 ) UNSIGNED NULL");
     $this->execute("ALTER TABLE `idea` CHANGE `deleted` `deleted` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0'");
--- protected/views/profile/projects.php
+++ protected/views/profile/projects.php
@@ -7,7 +7,7 @@
 <?php
 foreach($this->data['user']['idea'] AS $key => $idea){
 ?>
-    <div class="row panel" onclick="location.href='<?php echo Yii::app()->createUrl("idea/edit/{$key}"); ?>'">
+    <div class="row panel">
 
         <div class="edit-floater">
           
@@ -20,7 +20,47 @@ foreach($this->data['user']['idea'] AS $key => $idea){
         </div>        
 
         <a href="<?php echo Yii::app()->createUrl("idea/edit/{$key}"); ?>"><h5><?php echo $idea['title'];?></h5></a>
-        <small class="meta">created on <a><?php echo $idea['time_registered'];?></a> | has <a>3 members</a> | viewed <a>3 times</a></small>
+        <small class="meta">
+          <?php echo Yii::t('app', 'created on'); ?> <a><?php echo $idea['time_registered'];?></a> | 
+          <?php echo Yii::t('app', 'has <a>{n} member</a>| has <a>{n} members</a>',3); ?> | 
+          <?php echo Yii::t('app', 'viewed <a>{n} time</a>| viewed <a>{n} times</a>',3); ?>
+        </small>
+
+    </div>
+<?php
+}
+?>    
+    
+  </div>
+</div>
+
+<div class="row">
+  <div class="small-12 columns edit-header">
+    <h3><?php echo Yii::t('app', 'Projects I\'m member of'); ?></h3>
+  </div>
+  <div class="small-12 columns panel edit-content">
+    
+<?php
+foreach($this->data['user']['idea'] AS $key => $idea){
+?>
+    <div class="row panel">
+
+        <div class="edit-floater">
+          
+      <?php  echo CHtml::ajaxButton(Yii::t("app","Leave"),'','',
+                  array('class'=>"button tiny alert radius",
+                        'confirm'=>Yii::t("app","You are about to leave this project!\nAre you sure?"),
+                        'onclick'=>"$(document).stopPropagation();",
+                      )
+              ); ?>
+        </div>
+
+        <a href="<?php echo Yii::app()->createUrl("idea/edit/{$key}"); ?>"><h5><?php echo $idea['title'];?></h5></a>
+        <small class="meta">
+          <?php echo Yii::t('app', 'created on'); ?> <a><?php echo $idea['time_registered'];?></a> | 
+          <?php echo Yii::t('app', 'has <a>{n} member</a>| has <a>{n} members</a>',3); ?> | 
+          <?php echo Yii::t('app', 'viewed <a>{n} time</a>| viewed <a>{n} times</a>',3); ?>
+        </small>
 
     </div>
 <?php
