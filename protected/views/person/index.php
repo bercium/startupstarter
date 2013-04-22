aabce037d2c669202b9d58ee4c626ce93dc512b4
Ziga Berce <bercium@gmail.com>
2013-04-22 15:52:45 +0200

    User view card

--- protected/views/person/index.php
+++ protected/views/person/index.php
@@ -47,7 +47,7 @@ print_r($user);
       <small class="meta"><?php echo Yii::t('app','Colaboration:') ?> <a>monetary, cofinder</a></small><br />
       <small class="meta"><?php echo Yii::t('app','Available:') ?> <a>part time</a></small><br />
       <small class="meta"><?php echo Yii::t('app','Involved in') ?> <a><?php echo Yii::t('app','{n} project|{n} projects',array(3)) ?></a></small>
-      <div class="card-floater" style="border:none;">
+      <div class="card-floater" style="border:none;"> 
         <a class="small button success radius" style="margin-bottom:0;" href="<?php echo Yii::app()->createUrl("person/".$user['id']); ?>"><?php echo Yii::t('app','details...') ?></a>
       </div>
     </div>    
