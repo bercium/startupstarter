bed7938d61cf7ca2acbab90d0f33ab7894c24572
Ziga Berce <bercium@gmail.com>
2013-04-25 14:31:04 +0200

    Yii debug toolbar installed

--- protected/config/main.php
+++ protected/config/main.php
@@ -111,14 +111,16 @@ return array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
 				array(
-					'class'=>'CFileLogRoute',
+					//'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
+					'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+          'ipFilters'=>array('127.0.0.1'),
 				),
 				// uncomment the following to show log messages on web pages
-				
+				/*
 				array(
 					'class'=>'CWebLogRoute',
-				),
+				),*/
 				
 			),
 		),
