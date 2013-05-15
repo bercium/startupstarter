6973e0569a2c66ae1615769dcaad94c7f281075b
Jure <jure.ravlic@gmail.com>
2013-05-15 18:15:45 +0200

    idea page CSS
    maincontent. sidebar

--- protected/views/idea/view.php
+++ protected/views/idea/view.php
@@ -3,11 +3,11 @@ $this->pageTitle=Yii::app()->name;
 
 $idea = $data['idea'];
 ?>
-<div class="row">
+<div class="row idea-details">
 	<div class="large-12 small-12 columns radius panel card-idea">
 
 		<div class="row">
-			<div class="large-12 small-12 columns" >
+			<div class="large-12 small-12 columns title-field" >
 				<?php 
           $canEdit = false;
           foreach ($idea['member'] as $member){
@@ -25,31 +25,38 @@ $idea = $data['idea'];
 					</div>
 				<?php } ?>
 
-				<h5><?php echo $idea['title']; ?></h5>
+				<h1><?php echo $idea['title']; ?></h1>
 
 			</div>
 
-			<div class="large-12 small-12 columns panel" >
-        <?php if (count($idea['translation_other'])){
+			<div class="large-12 small-12 columns panel languages" >
+        <?php if (count($idea['translation_other'])){ ?>
+        <p class="meta">Languages:
+        <?php 
+          
           foreach ($idea['translation_other'] as $trans){
             echo '<a href="#">'.$trans['language']."</a> | ";
           }
         } ?>
+      </p>
 			</div>
 		</div>
 
 		<div  class="row">
 			<div class="large-8 small-12 columns">
-        <p>
+        <p class="pitch">
           <?php echo $idea['pitch']; ?>
         </p>
         
         <hr>
-        
+        <div class="meta-field">
+          <p class="meta">
         <?php echo Yii::t('app','Stage').": "; ?>
         <span data-tooltip title="<?php echo Yii::t('app',"Stage of project"); ?><br /><img src='<?php echo Yii::app()->request->baseUrl; ?>/images/stage-<?php echo $idea['status_id']; ?>.png'>">
           <a><?php echo $idea['status']; ?></a>
         </span>
+      </p>
+      <p class="meta">
         
         <?php 
           echo Yii::t('app','Members').": ";
@@ -68,12 +75,16 @@ $idea = $data['idea'];
               if (count($idea['member']) > 3) echo '<font class="meta">+'.(count($idea['member'])-3).'</font>';
           }
         ?>
-        <small class="meta">
+      </p>
+      <p>
+        <span class="meta">
         <?php echo Yii::t('app','Last updated').": ".$idea['date_updated']; ?>
-        </small>
+        </span>
+      </p>
+      </div>
         <hr>
         
-        <p class="meta">
+        <p class="">
           <?php 
             if ($idea['description_public']) echo $idea['description'];
             else Yii::t('msg',"Description isn't published!");
@@ -83,25 +94,32 @@ $idea = $data['idea'];
 			</div>
 			
 			<div class="large-4 small-12 columns">
-        <?php if ($idea['website']){
-          echo Yii::t('app',"Official web page").': <a href="http://'.$idea['website'].'" target="_blank">'.$idea['website']."</a><hr>";
+        <?php if ($idea['website']){ ?>
+        <p class="title-field meta"><span class="general foundicon-globe" title=""></span>
+        <?php
+          echo Yii::t('app',"Official web page").': <a href="http://'.$idea['website'].'" target="_blank">'.$idea['website']."</a>";
         } ?>
         <?php if (isset($idea['video']) && $idea['video']){
-          echo Yii::t('app',"Link to video").': <a href="http://'.$idea['video'].'" target="_blank">'.$idea['video']."</a><hr>";
+          echo Yii::t('app',"Link to video").': <a href="http://'.$idea['video'].'" target="_blank">'.$idea['video']."</a>";
         } ?>
-        
+        </p>
+        <hr>
         <?php if (count($idea['member']) > 0){ ?>
-        <h6 class="meta-title"><?php echo Yii::t('app','Looking for'); ?>: </h6>
+        <h6 class="meta-title title-field"><?php echo Yii::t('app','Looking for'); ?>: </h6>
 
           <?php
           $cnum = 0;
           foreach ($idea['member'] as $member){
             $cnum++; 
           ?>
-           <div  class="row">
+           <div  class="idea-sidebar">
             <div class="small-12 columns panel">
-             <h7><?php echo Yii::t('app','Candidate #{n}',$cnum); ?></h7>
-             <br /><br />
+              <?php if ($member['available_name']) { ?>
+                <p class="meta available-time button small secondary"><?php echo $member['available_name']; ?></p>
+              <?php } ?>
+             <h6><?php echo Yii::t('app','Candidate #{n}',$cnum); ?></h6>
+              
+
              <small class="meta person-skills">
                 <?php
                 foreach ($member['skill'] as $skill) {
@@ -112,7 +130,7 @@ $idea = $data['idea'];
                 ?>
             </small>
              
-            <br />
+           
             
               <?php if (count($member['collabpref']) > 0) { ?>
                 <small class="meta">
@@ -120,22 +138,17 @@ $idea = $data['idea'];
                     $firsttime = true;
                     if (is_array($member['collabpref']))
                       foreach ($member['collabpref'] as $collab) {
-                        if (!$firsttime) echo ", ";
-                        $firsttime = false;
-                        echo "<h7 class='meta-title'>".$collab['name']."<h7>";
+                        //if (!$firsttime) echo ", ";
+                        //$firsttime = false;
+                        echo "<h7 class='meta-title'>".$collab['name']."<h7> <br/>";
                       }
                     ?>
                 </small>
               <?php } ?>
-                
-              <?php if ($member['available_name']) { ?>
-                <h7><?php echo $member['available_name']; ?><h7>
-              <?php } ?>
-              
-              <br />
+
               
               <?php	if ($member['city'] || $member['country']){ ?>
-              <br />
+              <br>
                 <small class="meta" data-tooltip title="<img src='<?php echo getGMap($member['country'],$member['city'],$member['address']); ?>'>">
                 <span class="general foundicon-location" title=""></span>
               <a><?php
