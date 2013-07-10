e820e705b3ba8a1d173cb36427753cba2783899a
blazblaz <blaz.be@gmail.com>
2013-07-10 19:38:50 +0200

    IdeaClick, UserClick 1h

--- protected/components/Click.php
+++ protected/components/Click.php
@@ -7,29 +7,30 @@ class Click {
 	//also, we wish to keep the main function simple
 	public function idea($idea_click_id, $user_id = NULL){
 
-		//if(!isset(Yii::app()->session['idea_click'][$idea_click_id]) OR time() - 3600 > Yii::app()->session['idea_click'][$idea_click_id] ){
-			$click = new ClickIdea;
-			if($user_id == 0)
-				$user_id = NULL;
+		$click = new ClickIdea;
+		if($user_id == 0)
+			$user_id = NULL;
 
+		if( !isset($_SESSION['ideaClick'][$idea_click_id]) || 
+			( isset($_SESSION['ideaClick'][$idea_click_id]) && $_SESSION['ideaClick'][$idea_click_id] < time() - 3600) ){
+			
+			$_SESSION['ideaClick'][$idea_click_id] = time();
 			$click->setAttributes( array( 'user_id' => $user_id, 'idea_click_id' => $idea_click_id ) );
 			$click->save();
-
-			/*$array['idea_click'][$idea_click_id] = time();
-			Yii::app()->session = $array;
-		//}*/
+		}
 	}
 	public function user($user_click_id, $user_id = NULL){
 
-		//if(!isset(Yii::app()->session['user_click'][$user_click_id]) OR time() - 3600 > Yii::app()->session['user_click'][$user_click_id] ){
-			$click = new ClickUser;
-			if($user_id == 0)
-				$user_id = NULL;
+		$click = new ClickUser;
+		if($user_id == 0)
+			$user_id = NULL;
 
-			$click->findByAttributes( array( 'user_id' => $user_id, 'user_click_id' => $user_click_id ) );
+		if( !isset($_SESSION['userClick'][$user_click_id]) || 
+			( isset($_SESSION['userClick'][$user_click_id]) && $_SESSION['userClick'][$user_click_id] < time() - 3600) ){
+			
+			$_SESSION['userClick'][$user_click_id] = time();
+			$click->setAttributes( array( 'user_id' => $user_id, 'user_click_id' => $user_click_id ) );
 			$click->save();
-
-			/*Yii::app()->session['user_click'][$user_click_id] = time();
-		}*/
+		}
 	}
 }
