6829cebb0c64bf54d33a3b72b01a07c07c700ab5
Ziga Berce <bercium@gmail.com>
2013-10-08 13:05:02 +0200

    Small fixes

--- protected/config/main.php
+++ protected/config/main.php
@@ -117,7 +117,7 @@ return array(
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
-				/*array(
+				array(
 					//'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 					'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
