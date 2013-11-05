484bc7ffe454e9961b6d99b08cf8984252d4fd6b
Ziga Berce <bercium@gmail.com>
2013-11-05 23:27:37 +0100

    Fixes

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -20,11 +20,13 @@
 
 <?php //if (!Yii::app()->user->getState('fpi')){ ?>
 
+<?php if (Yii::app()->user->isGuest){ ?>
 <div  class="row" >
   <div class="column hide-for-small">
     <img class="logo image-beta" alt="Invite" title="invite" src="<?php echo Yii::app()->request->baseUrl; ?>/images/invite-<?php echo Yii::app()->getLanguage(); ?>.png" style="position: absolute; top: -4px; right:-10px; z-index: 98;" />
   </div>
 </div>
+<?php } ?>
 
 
 <div class="intro" <?php // if (isset($_GET['SearchForm'])) echo "style='display:none'"; ?>>
