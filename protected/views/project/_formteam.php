68e4d22f988c58822e11104c2599f9f16abb6563
Ziga Berce <bercium@gmail.com>
2013-11-12 18:29:38 +0100

    Fixed wrong industry position

--- protected/views/project/_formteam.php
+++ protected/views/project/_formteam.php
@@ -88,7 +88,9 @@
     <span class="description">
        <?php echo Yii::t('msg','Select group which represents skills above the closest.'); ?>
     </span>
+    <?php echo CHtml::dropDownList('skillset', '', CHtml::listData(Skillset::model()->findAllTranslated(),'id','name'), array('empty' => '&nbsp;','style'=>'display:none','class'=>'skillset')); ?>
 
+      
     <?php echo '<label for="skill">'.Yii::t('app','Skill')."</label>";  ?> 
     <span class="description" >
        <?php echo Yii::t('msg','Name of skill your candidate should posess. You can write multiple skills for the same industry separated by commas.'); ?>
@@ -97,8 +99,6 @@
       </strong>
     </span>
     <?php echo CHtml::textField("skill","", array('maxlength' => 128,'class'=>'skill')); ?>
-      
-    <?php echo CHtml::dropDownList('skillset', '', CHtml::listData(Skillset::model()->findAllTranslated(),'id','name'), array('empty' => '&nbsp;','style'=>'display:none','class'=>'skillset')); ?>
   
     <?php echo CHtml::submitButton(Yii::t("app","Add skill"),
                     array('class'=>"button small success radius",
--- protected/views/project/_formteamedit.php
+++ protected/views/project/_formteamedit.php
@@ -87,6 +87,9 @@
     <span class="description">
        <?php echo Yii::t('msg','Select group which represents skills above the closest.'); ?>
     </span>
+
+    <?php echo CHtml::dropDownList('skillset', '', CHtml::listData(Skillset::model()->findAllTranslated(),'id','name'), array('empty' => '&nbsp;','style'=>'display:none','class'=>'skillset')); ?>
+
       
     <?php echo '<label for="skill">'.Yii::t('app','Skill')."</label>";  ?> 
     <span class="description" >
@@ -96,9 +99,6 @@
       </strong>      
     </span>
     <?php echo CHtml::textField("skill","", array('maxlength' => 128,'class'=>'skill')); ?>
-  
-
-    <?php echo CHtml::dropDownList('skillset', '', CHtml::listData(Skillset::model()->findAllTranslated(),'id','name'), array('empty' => '&nbsp;','style'=>'display:none','class'=>'skillset')); ?>
   
     <?php echo CHtml::submitButton(Yii::t("app","Add skill"),
                     array('class'=>"button small success radius",
