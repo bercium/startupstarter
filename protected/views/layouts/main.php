591c1319148614062d09c627a5fb15027f14e40f
Ziga Berce <bercium@gmail.com>
2013-11-16 11:14:49 +0100

    Google analytics hide from develop

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -283,7 +283,7 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur
 })();
 </script> 
 
-
+<?php if (!YII_DEBUG){ ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -294,7 +294,8 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur
   ga('send', 'pageview');
 
 </script>
-
+<?php } ?>
+     
 </body>
 </html><?php 
     // be the last to override any other CSS settings
