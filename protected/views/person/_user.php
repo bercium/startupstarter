94e5103db50fc75c9899404cbc8f838e25a03557
Jure <jure.ravlic@gmail.com>
2013-11-20 09:28:17 +0100

    card-project, card-person

--- protected/views/person/_user.php
+++ protected/views/person/_user.php
@@ -1,20 +1,31 @@
 <div class="columns radius panel card-person">
     <div class="row card-person-title" onclick="window.open('<?php echo Yii::app()->createUrl("person/".$user['id']); ?>','_blank')">
       <div class="columns" >
-        <img src="<?php echo avatar_image($user['avatar_link'],$user['id'],60); ?>" style="height:36px; margin-right: 10px; float:left; margin-top:5px;" />
+        <img src="<?php echo avatar_image($user['avatar_link'],$user['id'],60); ?>" style="height:64px; margin-right: 10px; float:left; margin-top:5px;" />
         <h5><?php echo $user['name']." ".$user['surname']; ?></h5>
 				<?php	if ($user['city'] || $user['country']){ ?>
-						<small class="meta" data-tooltip title="<img src='<?php echo getGMap($user['country'],$user['city'],$user['address']); ?>'>">
+
+						<a href=""><small class="" data-tooltip title="<img src='<?php echo getGMap($user['country'],$user['city'],$user['address']); ?>'>">
            
-             <span class="icon-location" title=""></span>
+             
               <?php
                   echo $user['city']; 
                   if ($user['city'] && $user['country']) echo ', '; 
                   echo $user['country']; 
                   ?>
 						<?php //echo $user['address']; ?>
-						</small>
-					<?php } ?>				
+						</small><br></a>
+					<?php } ?>		
+                  
+                 <small><?php echo Yii::t('app','Available') ?>:
+                  <?php if ($user['available_name']) { ?>
+                     
+                  
+                    <?php echo $user['available_name']; ?>
+                    <?php } ?>               
+                    </small>
+
+                  	
 		  </div>
 	  </div>
     
@@ -25,50 +36,7 @@
 
       <div class="columns card-content"  >
         
-              <table border="0" width="300" cellpadding="0" cellspacing="0">
-                
-                <tr class="card-abstract">
-                  <td class="icons"><?php if (count($user['collabpref']) > 0){ ?>
-                     
-                      <span class="icon-group ico-awesome"></span>
-                      
-                      <?php } ?>
-                    
-                  </td>
-                  <td><?php 
-                    $firsttime = true;
-                    if (is_array($user['collabpref']))
-                    foreach ($user['collabpref'] as $collab){ 
-                      if (!$firsttime) echo ", ";
-                      $firsttime = false;
-                      echo $collab['name'];
-                    }
-                   ?><br />
-                  </td>
-                </tr>
-                <tr>
-                  <td class="icons"><?php if ($user['available_name']) { ?>
-                     <span class="icon-time ico-awesome"></span>
-                  </td>
-                  <td>
-                    <?php echo $user['available_name']; ?><br />
-                    <?php } ?>               
-
-                  </td>
-                </tr>
-                <tr>
-                  <td class="icons">
-                    
-                    <?php if ($user['num_of_rows']) { ?>
-                    <span class="icon-lightbulb ico-awesome"></span>
-                  </td>
-                  <td>
-                    <?php echo Yii::t('app','{n} project|{n} projects',array($user['num_of_rows'])) ?>
-                    <?php } ?>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="icons">
+              
                     
                   <?php 
                       $skills = array();
@@ -89,12 +57,13 @@
                       }
                       
                       //echo Yii::t('app','Skill|Skills',array($c)).":"; 
+                    ?><small>
+                    <?php
                       if (count($skills) > 0){
-                        echo '<span class="icon-suitcase ico-awesome"></span>'; 
-                        ?>
+                        echo Yii::t('app','Skilled in')
+                        ?>:<br>
                        
-                  </td>
-                  <td>
+                  
                     <?php 
                      foreach ($skills as $skillset=>$skill){
                           ?>
@@ -104,15 +73,20 @@
                         }
                     
                     ?> 
-                   
-                </td>
-                </tr>
-              </table>     
-        
-        <div class="card-floater">
+
+                  </small>      
+       
+		  </div>
+       <div class="card-person-footer">
+          <div class="left"><small>  
+            <?php echo Yii::t('app','Working on') ?>
+            <?php if ($user['num_of_rows']) { ?>
+            <?php echo Yii::t('app','{n} project|{n} projects',array($user['num_of_rows'])) ?>
+            <?php } ?>
+            </small>
+          </div>
           <a class="tiny button secondary right radius" style="margin-bottom:0;" href="<?php echo Yii::app()->createUrl("person/".$user['id']); ?>" target="_blank"><?php echo Yii::t('app','details').' <span class="icon-angle-right"></span>'; ?></a>
         </div>
-		  </div>
 	  </div>
     
   </div>
