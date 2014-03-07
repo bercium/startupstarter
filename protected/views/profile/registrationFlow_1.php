32a379e099d1a22ddeb16267dc183d3a1ce5bbc8
bercium <bercium@gmail.com>
2014-03-07 18:04:36 +0100

    Register flow

---
+++ protected/views/profile/registrationFlow_1.php
@@ -0,0 +1,109 @@
+<?php 
+  $this->pageTitle = Yii::t('app', 'Thanks! Now show off yourself!');
+?>
+
+<script>
+	var skillSuggest_url = '<?php echo Yii::app()->createUrl("profile/suggestSkill",array("ajax"=>1,"key"=>substr($user->activkey,0,10),"email"=>$user->email)) ?>';
+	var skillRemove_url = '<?php echo Yii::app()->createUrl("profile/deleteSkill",array("key"=>substr($user->activkey,0,10),"email"=>$user->email)); ?>';
+	var citySuggest_url = '<?php echo Yii::app()->createUrl("site/suggestCity",array("ajax"=>1)) ?>';
+</script>
+
+
+<p>
+
+  <?php echo Yii::t('msg','We are really happy you have decided to join our community. We strive to offer high quality profiles and project. This is why we decide on per person basis if we approve your registration or not.'); ?>
+  
+  <br /><br />
+  <strong>
+    <?php echo Yii::t('msg','Let\'s see how dedicated you are.'); ?>
+  </strong>
+    
+</p>
+
+  <ul class="button-group radius left">
+    <?php $step = 1; if (isset($_GET['step'])) $step = $_GET['step']; ?>
+    <li><a class="button tiny <?php if ($step != 1) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>1)); ?>">1. <?php echo Yii::t('app','Personal'); ?></a></li>
+    <li><a class="button tiny <?php if ($step != 2) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>2)); ?>">2. <?php echo Yii::t('app','Skills'); ?></a></li>
+    <li><a class="button tiny <?php if ($step != 3) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>3)); ?>">3. <?php echo Yii::t('app','Done'); ?></a></li>
+  </ul>
+
+<hr>
+<br />
+
+  <?php echo CHtml::beginForm(Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>2)),'post',array('class'=>"custom",'id'=>'after_register_form')); ?>
+  
+  
+  <div class="row">
+      <div class="large-4 right columns">
+      <?php 
+       //echo Yii::app()->getBaseUrl(true)."/".Yii::app()->params['tempFolder'];
+         //echo "<img class='avatar' src='".avatar_image($user->avatar_link, $user->id)."'>";
+
+           $this->widget('ext.EAjaxUpload.EAjaxUpload', array(
+              'id'=>'image',
+              'config'=>array(
+                 'action'=>Yii::app()->createUrl('/profile/upload'),
+                 'allowedExtensions'=>array("jpg", "jpeg", "png"),
+                 'template'=> '<div class="qq-uploader">' .
+                     '<div class="qq-upload-drop-area avatar-drop-area"><span>'.Yii::t('msg','Drop file here to change your profile picture.').'</span></div>' .
+                     '<div class="qq-upload-button">
+                       <div class="avatar-loading"><span class="qq-upload-spinner"></span></div>
+                       <img class="avatar" src="'.avatar_image($user->avatar_link, $user->id, false).'" >
+                      <div class="button secondary radius small avatar-change">'.Yii::t('app','Change image').' <span class="icon-upload"></span></div> 
+                      <span class="icon-info-sign" style="color: inherit"></span><span class="description">'.Yii::t('msg','To change your picture, drag a new image on the top or click on the button').'</span>
+                      </div>' .
+                     '<div class="qq-upload-list" style="display:none"></div>' .
+                  '</div>',
+                 'sizeLimit'=>4*1024*1024,// maximum file size in bytes
+                 'onSubmit'=>"js:function(file, extension) { 
+                                $('avatar-loading').show();
+                              }",
+                 'onComplete'=>"js:function(file, response, responseJSON) {
+                                  $('.avatar').load(function(){
+                                    $('avatar-loading').hide();
+                                    $('.avatar').unbind();
+                                    $('#UserEdit_avatar_link').val(responseJSON['filename']);
+                                  });
+                                  $('.avatar').attr('src', '".Yii::app()->baseUrl."/".Yii::app()->params['tempFolder']."'+responseJSON['filename']);
+                                }",
+                 'messages'=>array(
+                    'typeError'=>Yii::t('msg',"{file} has invalid extension. Only {extensions} are allowed."),
+                    'sizeError'=>Yii::t('msg',"{file} is too large, maximum file size is {sizeLimit}."),
+                    'emptyError'=>Yii::t('msg',"{file} is empty, please select files again without it."),
+                    'onLeave'=>Yii::t('msg',"The files are being uploaded, if you leave now the upload will be cancelled."),
+                 ),
+              )
+         )); 
+
+       ?>
+      <?php echo CHtml::activeHiddenField($user,'avatar_link'); ?>
+      </div>
+      <div class="large-7 left columns">
+
+      <?php //if (!$user->surname){ ?>
+      <?php echo CHtml::activeLabelEx($user,'name'); ?>
+      <?php echo CHtml::activeTextField($user,"name", array('maxlength' => 128)); ?>
+      <?php //} ?>
+        
+      <?php echo CHtml::activeLabelEx($user,'surname'); ?>
+      <?php echo CHtml::activeTextField($user,"surname", array('maxlength' => 128)); ?>        
+        
+      <?php echo CHtml::activeLabelEx($match,'country_id'); ?>
+      <?php echo CHtml::activedropDownList($match, 'country_id', GxHtml::listDataEx(Country::model()->findAllAttributes(null, true)), array('empty' => '&nbsp;','style'=>'display:none')); ?>
+
+      <?php echo CHtml::activeLabelEx($match,'city'); ?>
+      <?php echo CHtml::activeTextField($match, 'city', array("class"=>"city")); ?>
+
+      <?php // echo CHtml::activeLabelEx($user,'address'); ?>
+      <?php // echo CHtml::activeTextField($user, 'address', array('maxlength' => 128)); ?>
+
+    </div>
+    
+   </div>
+<?php echo CHtml::endForm(); ?>		
+    
+
+    <?php echo CHtml::button(Yii::t("app","Next >>"),
+      array('class'=>"button success radius right",'onclick'=>"$('#after_register_form').submit();")
+      ); ?>
+
---
+++ protected/views/profile/registrationFlow_2.php
@@ -0,0 +1,93 @@
+<?php 
+  $this->pageTitle = Yii::t('app', 'Thanks! Now show off yourself!');
+  $baseUrl = Yii::app()->baseUrl; 
+    $cs = Yii::app()->getClientScript();
+
+    $cs->registerCssFile($baseUrl.'/css/tagmanager.css');
+    $cs->registerScriptFile($baseUrl.'/js/tagmanager.js');
+?>
+
+<script>
+	var skillSuggest_url = '<?php echo Yii::app()->createUrl("profile/suggestSkill",array("ajax"=>1,"key"=>substr($user->activkey,0,10),"email"=>$user->email)) ?>';
+	var citySuggest_url = '<?php echo Yii::app()->createUrl("site/suggestCity",array("ajax"=>1)) ?>';
+</script>
+
+
+
+ <ul class="button-group radius left">
+    <?php $step = 1; if (isset($_GET['step'])) $step = $_GET['step']; ?>
+    <li><a class="button tiny <?php if ($step != 1) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>1)); ?>">1. <?php echo Yii::t('app','Personal'); ?></a></li>
+    <li><a class="button tiny <?php if ($step != 2) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>2)); ?>">2. <?php echo Yii::t('app','Skills'); ?></a></li>
+    <li><a class="button tiny <?php if ($step != 3) echo "secondary"; ?>" href="<?php echo Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>3)); ?>">3. <?php echo Yii::t('app','Done'); ?></a></li>
+  </ul>
+
+<hr>
+<br />
+
+
+<br />
+
+
+  <?php echo CHtml::beginForm(Yii::app()->createUrl('/profile/registrationFlow',array("key"=>$_GET['key'],"email"=>$_GET['email'],"step"=>3)),'post',array('class'=>"custom",'id'=>'after_register_form')); ?>
+  
+  <p>
+
+  <?php echo CHtml::errorSummary($match,"<div data-alert class='alert-box radius alert'>",'</div>'); ?>
+
+   <?php 
+    $skillList = '';
+    if(isset($data['user']['skill'])){
+      foreach ($data['user']['skill'] as $skill){
+          $skillList .= $skill['skill'].', ';
+        }}
+    //hidden-skill
+    ?>
+    
+    <label for="skill">
+    <?php echo Yii::t('app','What are you good at');  ?> 
+    </label>
+    <span class="description">
+      <?php echo Yii::t('msg','We know you have some awesome skills so why not show them to the others. Add all the things you are really good at and do not limit yourself by writing only formal education or job specific subjects.'); ?>
+    </span>
+    <input type="text" name="skill" placeholder="<?php echo Yii::t('app','short skill tags');  ?>" value="<?php echo $skillList; ?>" class="tm-input skill"/>
+    <br />    
+    
+  <?php echo CHtml::activeLabelEx($match,'available'); ?>
+  <span class="description"><?php echo Yii::t('msg','Select how much time you have to work on projects.'); ?></span>
+    
+  <?php echo CHtml::activedropDownList($match, 'available', GxHtml::listDataEx(Available::model()->findAllTranslated(),"id","name"), array('empty' => '&nbsp;','style'=>'display:none')); ?>
+
+    <?php echo "<label>".Yii::t('app','Collaboration preferences')."</label>"; ?>
+
+    <span class="description">
+       <?php echo Yii::t('msg','What kind of Collaboration do you prefer when working on projects.'); ?>
+    </span>
+  <?php foreach ($data['user']['collabpref'] as $colabpref){ ?>
+    <label for="CollabPref_<?php echo $colabpref['collab_id']; ?>">
+     <?php echo CHtml::checkBox('CollabPref['.$colabpref['collab_id'].']',$colabpref['active'],array('style'=>'display:none')); ?>
+     <?php echo $colabpref['name'] ?></label>
+    <span class="description">
+       <?php 
+       switch ($colabpref['collab_id']){
+         case 1:echo Yii::t('msg','Get paid for your work'); break;
+         case 2:echo Yii::t('msg','Are prepared to work for a share in a company'); break;
+         case 3:echo Yii::t('msg','Will work and invest equally'); break;
+         case 4:echo Yii::t('msg','Want to invest in interesting projects only'); break;
+         case 5:echo Yii::t('msg','Just want to help'); break;
+       }
+        ?>
+    </span>
+  
+     <?php
+  }
+
+  ?><br />
+  </p>
+  
+  
+<?php echo CHtml::endForm(); ?>		
+    
+  
+    <?php echo CHtml::button(Yii::t("app","Next >>"),
+      array('class'=>"button success radius right",'onclick'=>"$('#after_register_form').submit();")
+      ); ?>
