b63b11173de977781233b14689b1fc00d60f6ef4
Blaž Beuermann <blaz.be@gmail.com>
2014-03-15 19:59:26 +0100

    Project gallery in first step

--- protected/views/project/_formidea.php
+++ protected/views/project/_formidea.php
@@ -52,6 +52,50 @@ $lang = $lang->id;
 <?php echo CHtml::activeTextArea($translation, "pitch"); ?>
 <br />
 <br />
+
+        <div class="left large-4 small-4 columns" style="clear: both">
+            <?php
+            //echo Yii::app()->getBaseUrl(true)."/".Yii::app()->params['tempFolder'];
+            //echo "<img class='avatar' src='".avatar_image($user->avatar_link, $user->id)."'>";
+            $this->widget('ext.EAjaxUpload.EAjaxUpload', array(
+                'id' => 'image',
+                'config' => array(
+                    'action' => Yii::app()->createUrl('/project/upload'),
+                    'allowedExtensions' => array("jpg", "jpeg", "png"),
+                    'template' => '<div class="qq-uploader">' .
+                        '<div class="qq-upload-drop-area avatar-drop-area"><span>' . Yii::t('msg', 'Drop file here to upload a new cover image.') . '</span></div>' .
+                        '<div class="qq-upload-button">
+                          <div class="avatar-loading"><span class="qq-upload-spinner"></span></div>
+                          <img class="avatar" src="' . idea_image($ideagallery, $idea_id, false) . '" >
+                      <div class=" button disabled secondary radius small avatar-change">' . Yii::t('app', 'Add cover image') . ' <span class="icon-upload"></div>
+                      </div>' .
+                        '<div class="qq-upload-list" style="display:none"></div>' .
+                        '</div>',
+                    'sizeLimit' => 4 * 1024 * 1024, // maximum file size in bytes
+                    'onSubmit' => "js:function(file, extension) {
+                                $('avatar-loading').show();
+                              }",
+                    'onComplete' => "js:function(file, response, responseJSON) {
+                                  $('.avatar').load(function(){
+                                    $('avatar-loading').hide();
+                                    $('.avatar').unbind();
+                                    $('#IdeaImage_avatar_link').val(responseJSON['filename']);
+                                  });
+                                  $('.avatar').attr('src', '" . Yii::app()->baseUrl . "'+responseJSON['filename']);
+                                }",
+                    'messages' => array(
+                        'typeError' => Yii::t('msg', "{file} has invalid extension. Only {extensions} are allowed."),
+                        'sizeError' => Yii::t('msg', "{file} is too large, maximum file size is {sizeLimit}."),
+                        'emptyError' => Yii::t('msg', "{file} is empty, please select files again without it."),
+                        'onLeave' => Yii::t('msg', "The files are being uploaded, if you leave now the upload will be cancelled."),
+                    ),
+                )
+            ));
+
+            ?>
+            <input name="IdeaGallery[url]" id="IdeaImage_avatar_link" type="hidden" value="<?php echo $ideagallery; ?>"/>
+        </div>
+
     <div class="lin-trigger panel">
         <?php echo CHtml::activeLabelEx($idea, 'website'); ?>
         <div class="lin-hidden">
