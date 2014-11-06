44c289efdbb22bd2f0815b598d6505e787b9955f
Ziga Berce <bercium@gmail.com>
2014-11-06 12:02:14 +0100

    Double mailing disabled

--- protected/views/event/signupStep3.php
+++ protected/views/event/signupStep3.php
@@ -20,8 +20,12 @@ echo "<h3>".Yii::t('msg',"You've been successfully signed up for the event.")."<
 
 <br />
 <?php 
-  echo "<h3>".Yii::t('msg',"Most shares on Twitter will get you on this event for FREE!")."</h3>"; 
+  //echo "<h3>".Yii::t('msg',"Most shares on Twitter will get you on this event for FREE!")."</h3>"; 
+/*
 ?>
 
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://bit.ly/1igkQBe" data-text="Naslednji dogodek Sestavi svojo ekipo - Internet of things! 9.5. @Hekovnik in @cofinder_eu #cofindersseiot" data-size="large">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<?php 
+  */
+?>
