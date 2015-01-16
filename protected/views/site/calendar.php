7341652e44abf4293b9b4fa6acebf402ff3b09ab
Ziga Berce <bercium@gmail.com>
2015-01-16 12:08:27 +0100

    Events page updated

--- protected/views/site/calendar.php
+++ protected/views/site/calendar.php
@@ -125,8 +125,8 @@ $cs->registerScriptFile($baseUrl.'/js/fullcalendar/fullcalendar.min.js');
       ?>
         <h2 class="f-" style="text-transform:none"><?php echo $event->title; ?></h2>
         <small>
-          <div><?php echo Yii::app()->dateFormatter->formatDateTime(strtotime($event->start), "medium", null); ?> 
-              <span class="mb8 meta left" style="font-weight: bold;" data-tooltip title="<img src='<?php echo getGMap($event->country,$event->city,''); ?>'>" ><?php echo $event->location; ?>
+          <div class="mb8" ><?php echo Yii::app()->dateFormatter->formatDateTime(strtotime($event->start), "medium", null); ?> 
+              <span class="meta" style="font-weight: bold;" data-tooltip title="<img src='<?php echo getGMap($event->country,$event->city,''); ?>'>" ><?php echo $event->location; ?>
               </span>
           </div>
           <p><?php echo $event->content; ?></p>
