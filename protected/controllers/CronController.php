50de64c7e4d68d787a7712d908650006283f7998
Ziga Berce <bercium@gmail.com>
2015-09-08 21:22:31 +0200

    Test

--- protected/controllers/CronController.php
+++ protected/controllers/CronController.php
@@ -98,42 +98,59 @@ class CronController extends Controller
    * test 
    */
   public function actionNotifyTest(){
-    $message = new YiiMailMessage;
+   $message = new YiiMailMessage;
     $message->view = 'system';
     $message->from = Yii::app()->params['noreplyEmail'];
     
     // send newsletter to all in waiting list
     $hidden = UserStat::model()->findAll("completeness < :comp",array(":comp"=>PROFILE_COMPLETENESS_MIN));
+
     foreach ($hidden as $stat){
       //set mail tracking
-      if ($stat->user->status == 0) continue; // skip non active users
-      if (strtotime($stat->user->lastvisit_at) < strtotime('-2 month')) continue; // skip users who haven't been on our platform for more than 2 moths
-      
-      //echo $stat->user->email." - ".$stat->user->name." ".$stat->user->surname." ".$stat->user->lastvisit_at." your profile is not visible!";
+        echo "1";
+      if ($stat->user->status != 0) continue; // skip active users
+      echo "2";
+      if ($stat->user->newsletter == 0) continue; // skip those who unsubscribed
+      echo "3";
+      if ($stat->user->lastvisit_at != '0000-00-00 00:00:00') continue; // skip users who have already canceled their account
+      echo "4";
+      //echo $stat->user->name." - ".$stat->user->email.": ".$stat->user->create_at." (".date('c',strtotime('-4 week'))."     ".date('c',strtotime('-3 week')).")<br />\n";
+      $create_at = strtotime($stat->user->create_at);
+      if ($create_at < strtotime('-8 week') || $create_at >= strtotime('-1 day')) continue;      
+      if (!
+          (($create_at >= strtotime('-1 week')) || 
+          (($create_at >= strtotime('-4 week')) && ($create_at < strtotime('-3 week'))) || 
+          (($create_at >= strtotime('-8 week')) && ($create_at < strtotime('-7 week'))) )
+         ) continue;
+      echo "5";
+      //echo $stat->user->email." - ".$stat->user->name." your Cofinder profile is moments away from approval!";
+
+      //echo "SEND: ".$stat->user->name." - ".$stat->user->email.": ".$stat->user->create_at." (".$stat->completeness.")<br />\n";
+      //echo 'http://www.cofinder.eu/profile/registrationFlow?key='.substr($stat->user->activkey,0, 10).'&email='.$stat->user->email;
+
       //continue;
-      
+      //set mail tracking
       $mailTracking = mailTrackingCode($stat->user->id);
       $ml = new MailLog();
       $ml->tracking_code = mailTrackingCodeDecode($mailTracking);
-      $ml->type = 'hidden-profiles';
+      $ml->type = 'registration-flow-reminder';
       $ml->user_to_id = $stat->user->id;
       $ml->save();
-      
+
       $email = $stat->user->email;
       $email = 'bercium@gmail.com';
-      $message->subject = $stat->user->name." your Cofinder profile is hidden!";
-      
-      $content = 'Your profile on Cofinder is not visible due to lack of information you provided. 
-                  If you wish to be found we suggest you take a few minutes and '.
-              mailButton("fill it up", absoluteURL('/profile'),'success',$mailTracking,'fill-up-button');
-      
+      $message->subject = $stat->user->name." your Cofinder account almost approved"; // 11.6. title change
+
+      $content = "We couldn't approve your profile just yet since you haven't provided enough information."
+              . "Please fill your profile and we will revisit your application.".
+              mailButton("Do it now", absoluteURL().'/profile/registrationFlow?key='.substr($stat->user->activkey,0, 10).'&email='.$stat->user->email,'success',$mailTracking,'fill-up-button');
+
       $message->setBody(array("content"=>$content,"email"=>$email,"tc"=>$mailTracking), 'text/html');
       $message->setTo($email);
-      var_dump($content);
-      Yii::app()->mail->send($message);
-
-      Notifications::setNotification($stat->user_id,Notifications::NOTIFY_INVISIBLE);
+      //Yii::app()->mail->send($message);
       return;
+      Notifications::setNotification($stat->user_id,Notifications::NOTIFY_INVISIBLE);      
+
     }
     return 0;
   }
