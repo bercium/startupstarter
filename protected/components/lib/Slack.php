64dfe2029812d9de57cccccbf6b60f709bac9fc0
Ziga Berce <bercium@gmail.com>
2015-09-08 22:03:07 +0200

    Slack message integration

---
+++ protected/components/lib/Slack.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Slack
+{
+  
+    public static function message($message, $icon = "", $room = "activity") {
+        $room = ($room) ? $room : "engineering";
+        $data = "payload=" . json_encode(array(
+                "channel"       =>  "#{$room}",
+                "text"          =>  $message,
+                "icon_emoji"    =>  $icon
+            ));
+	
+	// You can get your webhook endpoint from your Slack settings
+        $ch = curl_init("https://cofinder.slack.com/services/hooks/slackbot?token=UdtpiGlSqC5VcjSm2O0xotwV");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+	
+	// Laravel-specific log writing method
+        // Log::info("Sent to Slack: " . $message, array('context' => 'Notifications'));
+        return $result;
+    }
+    
+}
